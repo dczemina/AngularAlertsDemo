722c71851e6e5f5cf6782b58d34c1e9c57906b98
David Zemina <35789015+dczemina@users.noreply.github.com>
2023-06-15 21:40:27 -0400

    Add SomeFeatureModule
    Add SomeFeatureModule which will contain a dummy feature for the demo

--- src/app/some-feature/some-feature.module.ts
+++ src/app/some-feature/some-feature.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+import { SomeComponent } from './some/some.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SomeComponent
+  ],
   imports: [
-    CommonModule
+    // Official Modules
+    CommonModule,
+    // Custom Modules
+    SharedModule
+  ],
+  exports: [
+    SomeComponent
   ]
 })
 export class SomeFeatureModule { }
